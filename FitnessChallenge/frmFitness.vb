3259669b5eb07d0a673222938aff829b5a056e2d
PatrickALove <PatrickALove@users.noreply.github.com>
2019-04-16 13:14:59 -0400

    Initial commit

---
+++ FitnessChallenge/frmFitness.vb
@@ -0,0 +1,90 @@
+﻿' Program Name: Fitness Challenge
+' Author      : Patrick Andrew Love
+' Student ID  : 1683445
+' Date        : 02/26/19
+' Time        : 1:04 pm
+' Purpose     : The Fitness Challenge Program enters the weight loss
+'               from team members for a fitness challenge. It displays each
+'               weight loss value. After all weight loss values have been entered,
+'               it displays the average eight loss for the team.
+
+Option Strict On
+
+
+Public Class frmFitness
+    Private Sub btnWeightLoss_Click(sender As Object, e As EventArgs) Handles btnWeightLoss.Click
+        ' the btnWeightLoss_Click event accepts and displays up to 8 weight loss values
+        ' and then calculates and displays the average weight loss for the team
+
+        'Declare and initialize variables
+        Dim strWeightLoss As String
+        Dim decWeightLoss As Decimal
+        Dim decAverageLoss As Decimal
+        Dim decTotalWeightLoss As Decimal = 0D
+        Dim strInputMessage As String = "Please enter the weight loss for team member #"
+        Dim strInputHeading As String = "Weight Loss"
+        Dim strNormalMessage As String = "Enter the weight loss for team member #"
+        Dim strNonNumericError As String = "Error- Please enter a number for the weight loss of team member #"
+        Dim strNegativeError As String = "Error- Please enter a positive number for the weight loss of team member #"
+
+        'Declare and initialize loop variables
+        Dim strCancelClicked As String = ""
+        Dim intMaxNumberOfEntries As Integer = 8
+        Dim intNumberOfEntries As Integer = 1
+
+        'This loop allows the user to enter the weight loss of up to 8 team members.
+        'the loop terminates when the user has entered 8 weight loss values or the user
+        'clicks the Cancel button or the Close button in the InputBox
+        strWeightLoss = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+
+        Do Until intNumberOfEntries > intMaxNumberOfEntries Or strWeightLoss = strCancelClicked
+            If IsNumeric(strWeightLoss) Then
+                decWeightLoss = Convert.ToDecimal(strWeightLoss)
+                If decWeightLoss > 0 Then
+                    lstWeightLoss.Items.Add(decWeightLoss)
+                    decTotalWeightLoss += decWeightLoss
+                    intNumberOfEntries += 1
+                    strInputMessage = strNormalMessage
+                Else
+                    strInputMessage = strNegativeError
+                End If
+            Else
+                strInputMessage = strNonNumericError
+            End If
+            If intNumberOfEntries <= intMaxNumberOfEntries Then
+                strWeightLoss = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+            End If
+        Loop
+        'Calculates and displays average team weight loss
+        If intNumberOfEntries > 1 Then
+            lblAverageLoss.Visible = True
+            decAverageLoss = decTotalWeightLoss / (intNumberOfEntries - 1)
+            lblAverageLoss.Text = "Average weight loss is " &
+                decAverageLoss.ToString("F1") & " lbs"
+        Else
+            MsgBox("No weight loss value entered")
+        End If
+
+        'disables the weight loss button
+        btnWeightLoss.Enabled = False
+    End Sub
+
+    Private Sub mnuClear_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+        ' The mnuClear_Click event clears the ListBox object and hides
+        ' the average weight loss label. It also enables the Weight Loss button
+
+        lstWeightLoss.Items.Clear()
+        lblAverageLoss.Visible = False
+        btnWeightLoss.Enabled = True
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        ' mnuExit_Click event closes the window and exits the application
+
+        Close()
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
